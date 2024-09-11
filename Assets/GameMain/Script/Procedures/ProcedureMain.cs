c9f66d2782affaa593f7d44933aa69e7afe15688
周尚礼 <105686154+zsl2021533013@users.noreply.github.com>
2024-09-11 23:51:06 +0800

    修改

--- Assets/GameMain/Script/Procedures/ProcedureMain.cs
+++ Assets/GameMain/Script/Procedures/ProcedureMain.cs
@@ -156,8 +156,8 @@ namespace GameMain.Scripts.Procedure
             {
                 ChangeSceneState.nextState = ProcedureStates.Game;
                 
-                ChangeSceneState.nextScenePath = PathManager.GetLevelAsset("2-13");
-                GameState.currentScene = GameState.sceneList.Find(config => config.sceneNumber == "2-13");
+                ChangeSceneState.nextScenePath = PathManager.GetLevelAsset("2-10");
+                GameState.currentScene = GameState.sceneList.Find(config => config.sceneNumber == "2-10");
                 
                 mFSM.ChangeState(ProcedureStates.ChangeScene);
             });
